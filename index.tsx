e3a8e1a3d92c5522e27ce50e25db73cad8dbd02c
ziroolga-glitch <ziro.olga@gmail.com>
2026-02-01 20:36:55 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,13 +4,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
